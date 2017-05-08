0190ba911d78ee00c9f17f510d1ac2b5346be1b5
workstation-1 <workstation-1@automate-demo.com>
2017-05-08 10:26:12 +0000

    add build cookbook

---
+++ recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: bjc-ssl
+# Recipe:: ssl
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'apache2'
+
+template '/etc/apache2/sites-available/default-ssl.conf' do
+  action :create
+  source 'default-ssl.conf.erb'
+  notifies :restart, 'service[apache2]', :immediately
+end
+
+service 'apache2' do
+  action :nothing
+end
+
+execute 'a2enmod ssl'
+
+execute 'a2ensite default-ssl'
